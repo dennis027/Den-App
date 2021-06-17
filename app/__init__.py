5ef788878a265dd016ace5b67b534120d814d559
Dennis <dennis.kimani@student.moringaschool.com>
2021-06-17 20:29:43 +0300

    updating a create app instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,45 +1,36 @@
-from flask import Flask
+from flask import Flask 
 from .config import config_options
+from flask_mail import Mail
+from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_uploads import UploadSet,configure_uploads,IMAGES
-from flask_mail import Mail
+from flask_uploads import IMAGES, UploadSet,configure_uploads
 
-bootstrap = Bootstrap()
-login_manager=LoginManager()
-login_manager.session_protection='strong'
-login_manager.login_view ='auth.login'
 db = SQLAlchemy()
+mail = Mail()
+bootstap = Bootstrap()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
-mail= Mail()
+
+
+
 def create_app(config_name):
     app = Flask(__name__)
-
-#setting up configuration
     app.config.from_object(config_options[config_name])
-
-#initializing flask extensions
-    bootstrap.init_app(app)
-    db.init_app(app)
-    login_manager.init_app(app)
-
-    # registering a blueprint
-
+    from .auth import auth as authentication_blueprint
     from .main import main as main_blueprint
+
+    app.register_blueprint(authentication_blueprint)
     app.register_blueprint(main_blueprint)
-# setting config
-    # from .request import config_request
-    # configure_request(app)
-    from.auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix='/authenticate')
-    #CONFIG UPLOAD
+
+    login_manager.init_app(app)
+    db.init_app(app)
+    bootstap.init_app(app)
     configure_uploads(app,photos)
-    # MAIL
     mail.init_app(app)
+    
+
 
     return app
-
-
-# from app import views
-# from app import error
