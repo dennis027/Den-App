f7d49e5f81a87f1b089dba6835cee17073fba95d
Dennis <dennis.kimani@student.moringaschool.com>
2021-06-17 20:31:58 +0300

    adding all my classes for the app

--- app/models.py
+++ app/models.py
@@ -1,77 +1,135 @@
-
-from flask_script import Manager
 from . import db,login_manager
+from datetime import datetime
+from flask_login import UserMixin,current_user
 from werkzeug.security import generate_password_hash,check_password_hash
-from flask_login import UserMixin
-class Review:
-    all_reviews = []
 
-    def __init__(self,pitches_id,title,imageurl,review):
-        self.pitches_id = pitches_id
-        self.title = title
-        self.imageurl = imageurl
-        self.review = review
-
-    def save_reviews(self):
-        Review.all_reviews.append(self)
-
-
-    @classmethod
-    def clear_reviews(cls):
-        Review.all_reviews.clear()            
-    @classmethod
-    def get_reviews(cls,id):
-        response =[]
-        for review in cls.all_reviews:
-            if review.poster_id == id:
-                response.append(review)
-        return response  
-
-class User(UserMixin,db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255),index=True)
-    email = db.Column(db.String(255), index=True)
-    role_id=db.Column(db.Integer, db.ForeignKey('roles.id'))
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255),unique = True,nullable = False)
+    email  = db.Column(db.String(255),unique = True,nullable = False)
+    secure_password = db.Column(db.String(255),nullable = False)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
-    pass_secure=db.Column(db.String(255))
+    pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
+    upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='user',lazy='dynamic')
+    
+
     @property
-    def password(self):
+    def set_password(self):
         raise AttributeError('You cannot read the password attribute')
 
-    @password.setter
+    @set_password.setter
     def password(self, password):
-        self.pass_secure = generate_password_hash(password)
+        self.secure_password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.secure_password,password) 
+    
+    def save_u(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def __repr__(self):
+        return f'User {self.username}'
+class Roles(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f'User {self.name}' 
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255),nullable = False)
+    posted = db.Column(db.Text(), nullable = False)
+    # post = db.Column(db.Text(), nullable = False)
+    comment = db.relationship('Comment',backref='pitch',lazy='dynamic')
+    upvote = db.relationship('Upvote',backref='pitch',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='pitch',lazy='dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    category = db.Column(db.String(255), index = True,nullable = False)
+    
+    def save_p(self):
+        db.session.add(self)
+        db.session.commit()
+
+        
+    def __repr__(self):
+        return f'Pitch {self.post}'
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text(),nullable = False)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable = False)
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'),nullable = False)
+
+    def save_c(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,pitch_id):
+        comments = Comment.query.filter_by(pitch_id=pitch_id).all()
+
+        return comments
+
+    
+    def __repr__(self):
+        return f'comment:{self.comment}'
+
+class Upvote(db.Model):
+    __tablename__ = 'upvotes'
+
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_upvotes(cls,id):
+        upvote = Upvote.query.filter_by(pitch_id=id).all()
+        return upvote
 
 
-    def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
+    def __repr__(self):
+        return f'{self.user_id}:{self.pitch_id}'
+class Downvote(db.Model):
+    __tablename__ = 'downvotes'
+
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    @classmethod
+    def get_downvotes(cls,id):
+        downvote = Downvote.query.filter_by(pitch_id=id).all()
+        return downvote
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.pitch_id}'
+
        
-
-    def __repr__(self):
-        return f'user {self.username}'  
-
-
-
-#....
-# @manager.shell
-# def make_shell_context():
-#     return dict(app = app,db = db,User = User )
-# if __name__ == '__main__':
-#     manager.run()
-
-class Role(db.Model):
-    __tablename__='roles'
-
-    id= db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String(255))
-    user= db.relationship('User', backref= 'role',lazy="dynamic")
-
-    def __repr__(self):
-        return f'User{self.name}'
-
-
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))        
+    return User.query.get(user_id)
